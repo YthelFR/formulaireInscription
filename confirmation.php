86ab8736a5a813117300f82066c31f4a8c5f8dbb
YoanJ <110157294+YthelFR@users.noreply.github.com>
2024-02-19 13:20:08 +0100

    Add files via upload

---
+++ confirmation.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmation</title>
+    <link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+    <form action="./src/authentication.php">
+        <h1>Merci de votre inscription !</h1>
+        <label for="mail"></label>
+        <input type="email" name="mail" id="mail">
+        <label for="password"></label>
+        <input type="text" name="password" id="password">
+        <input type="submit" value="">
+    </form>
+</body>
+</html>
